5b62ae9c37e01f0deba709e88235506cce4368d6
Stephen Shkardoon <ss23@ss23.geek.nz>
2012-05-30 15:38:53 +1000

    Added a new default config for routes

--- site/app/Config.default/routes.php
+++ site/app/Config.default/routes.php
@@ -32,6 +32,10 @@
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
+ * Fancy rule for making songs nice
+ */
+	Router::connect('/:controller/:id/:slug', array('action' => 'view'), array('pass' => array('id', 'slug'), 'id' => '[0-9]+'));
+/**
  * Load all plugin routes.  See the CakePlugin documentation on 
  * how to customize the loading of plugin routes.
  */
