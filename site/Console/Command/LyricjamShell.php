8c2039676071ae93c404e039008b1c726ab4af09
Stephen Shkardoon <ss23@ss23.geek.nz>
2013-09-05 01:49:20 +1200

    Beginnings of a sitemap generator, needs work

--- site/Console/Command/LyricjamShell.php
+++ site/Console/Command/LyricjamShell.php
@@ -44,4 +44,40 @@ class LyricjamShell extends AppShell {
 		$this->out("Done.");
 	}
 
+	public function generateSitemap() {
+		// There has to be some way to get this automatically, but...
+		$pages = array();
+		$pages[] = array('loc' => '/', 'changefreq' => 'hourly', 'priority' => '1.0');
+		foreach (array('about', 'apidocs') as $page) { // Static pages
+			$pages[] = array('location' => '/pages/' . $page, 'changefreq' => 'weekly', 'priority' => '0.5');
+		}
+
+		// Next, add every static page for artists/songs/albums
+		foreach (array('artists', 'songs', 'albums') as $page) {
+			// Perhaps should add a new page here for every pagination page
+			$pages[] = array('location' => '/' . $page, 'changefreq' => 'weekly', 'priority' => '0.2');
+			// Do we still have "hot" artists etc? I think it's a nice feature, but it might as well
+			// replace the entire index controller eventually
+		}
+
+		// Generate the XML
+		file_put_contents('/tmp/lj-sitemap.xml', $this->generateXML($pages));
+		$this->out("Done.");
+	}
+
+	protected function generateXML($pages) {
+		$xml = <<<XML
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+XML;
+		foreach ($pages as $page) {
+			$xml .= "<url>";
+			foreach ($page as $key => $value) {
+				$xml .= "<" . $key . ">" . $value . "</" . $key . ">";
+			}
+			$xml .= "</url>";
+		}
+		$xml .= "</urlset>";
+		return $xml;
+	}
+
 }
