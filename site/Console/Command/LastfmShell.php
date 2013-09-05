a332b74c639f3583133f491ca5e0068ec4110d07
Thomas Edvalson <machin3@gmail.com>
2013-09-05 13:46:50 -0400

    Added argument so you can sync with last.fm based on a single artist

--- site/Console/Command/LastfmShell.php
+++ site/Console/Command/LastfmShell.php
@@ -5,13 +5,33 @@ class LastfmShell extends AppShell {
 	public $uses = array('Album','Artist','Song');
 	
 	public function sync() {
+		$artist_name = (count($this->args) > 0) ? $this->args[0] : null;
 		// TODO: Utilize all musicbrainz uuids in json to sync and update our db
 		$this->out('<info>Syncing with Last.fm for album art and Musicbrainz UUIDs ...</info>');
-		$this->Album->recursive = -1;
+		// If artist argument supplied, sync only albums for that artist, else sync all albums
 		// Fetch albums with no art, no recursion for speed and memory reasons
-		$albums = $this->Album->find('all', array(
-			'conditions' => array('art' => '')
-		));
+		$this->Album->recursive = -1;
+		if ($artist_name) {
+			$this->Artist->recursive = 2;
+			$artist = $this->Artist->findByName($artist_name);
+			if (!$artist) {
+				throw new NotFoundException(__('Invalid artist'));
+			}
+			$album_ids = array_unique(Hash::extract($artist, 'Song.{n}.Album.{n}.id'));
+			$albums = $this->Album->find('all', array(
+					'fields' => array('DISTINCT id', 'name', 'slug', 'art'),
+					'conditions' => array(
+							'id' => $album_ids,
+							'art' => '',
+						)
+				)
+			);
+		} else {
+			$albums = $this->Album->find('all', array(
+					'conditions' => array('art' => '')
+			));
+		}
+		
 		$this->Album->recursive = 2;
 		foreach ($albums as $album){
 // 			if (!empty($album['Album']['uuid']))
