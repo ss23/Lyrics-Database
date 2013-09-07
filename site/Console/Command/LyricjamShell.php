a0fd542d3ba2596a0905d49a9cbd5bb6b231e923
Stephen Shkardoon <ss23@ss23.geek.nz>
2013-09-07 17:06:03 +1200

    Sitemap generation is now fixed, requires config

--- site/Console/Command/LyricjamShell.php
+++ site/Console/Command/LyricjamShell.php
@@ -55,16 +55,17 @@ class LyricjamShell extends AppShell {
 	public function generateSitemap() {
 		// There has to be some way to get this automatically, but...
 		$pages = array();
-		$pages[] = array('loc' => '/', 'changefreq' => 'hourly', 'priority' => '1.0');
+		$baseURL = Configure::read('base_url');
+		$pages[] = array('loc' => $baseURL . '/', 'changefreq' => 'hourly', 'priority' => '1.0');
 		foreach (array('/pages/about', '/pages/apidocs') as $page) { // Static pages
-			$pages[md5($page)] = array('location' => $page, 'changefreq' => 'weekly', 'priority' => '0.5');
+			$pages[md5($page)] = array('loc' => $baseURL . $page, 'changefreq' => 'weekly', 'priority' => '0.5');
 		}
 
 		// Next, add every static page for artists/songs/albums
 		foreach (array('artists', 'songs', 'albums') as $page) {
 			$this->out('Adding ' . $page);
 			// Perhaps should add a new page here for every pagination page
-			$pages[md5('/' . $page)] = array('location' => '/' . $page, 'changefreq' => 'weekly', 'priority' => '0.4');
+			$pages[md5('/' . $page)] = array('loc' => $baseURL . '/' . $page, 'changefreq' => 'weekly', 'priority' => '0.4');
 		}
 
 		// Add our real URLs
@@ -76,7 +77,7 @@ class LyricjamShell extends AppShell {
 			$this->out('.', 0);
 			foreach ($items as $item) {
 				foreach ($this->findURLsForSong($item) as $url) {
-					$pages[md5($url)] = array('location' => $url, 'changefreq' => 'monthly', 'priority' => '0.2');
+					$pages[md5($url)] = array('loc' => $baseURL . $url, 'changefreq' => 'monthly', 'priority' => '0.2');
 				}
 			}
 			$offset += $limit;
@@ -89,7 +90,8 @@ class LyricjamShell extends AppShell {
 
 	protected function generateXML($pages) {
 		$xml = <<<XML
-<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 XML;
 		foreach ($pages as $page) {
 			$xml .= "<url>";
